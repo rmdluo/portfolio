70bab1daa1d3ee7f255f66c40af26cc87d4ed99a
rmdluo <rmdluo@gmail.com>
2025-05-16 11:23:25 -0400

    add gallery

---
+++ src/data/gallery.ts
@@ -0,0 +1,14 @@
+import type { ImageCardProps } from "../types/ImageCardProps";
+
+export const images: ImageCardProps[] = [
+    {
+        src: "../src/assets/gallery/after/IMG_2727.jpg",
+        alt: "Gallery Image",
+        aspectRatio: "landscape"
+    },
+    {
+        src: "../src/assets/gallery/before/IMG_2727.jpg",
+        alt: "Gallery Image",
+        aspectRatio: "landscape"
+    }
+];
---
+++ src/types/ImageCardProps.ts
@@ -0,0 +1,10 @@
+export type AspectRatio = "square" | "landscape" | "portrait" | "wide" | "ultrawide";
+export type Size = "small" | "medium" | "large" | "full" | "dynamic";
+
+export interface ImageCardProps {
+    src: string;
+    alt: string;
+    caption?: string;
+    aspectRatio?: AspectRatio;
+    size?: Size;
+}
