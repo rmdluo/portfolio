a54209e87c0bc0a1a35d66c217eb5873ecffaabb
rmdluo <rmdluo@gmail.com>
2025-05-16 19:54:35 -0400

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
-// import image from '@astrojs/image';
-
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  },
-
-  // integrations: [image()]
+  }
 });
