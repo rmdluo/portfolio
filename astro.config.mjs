81916cff746c397b17d1a0d5d77a3365db484932
rmdluo <rmdluo@gmail.com>
2025-05-16 10:05:05 -0400

    add image cards

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import image from '@astrojs/image';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [image()]
 });
