a8e96812a98c9347087c3a4c1de00b889846cb21
rmdluo <rmdluo@gmail.com>
2025-05-16 19:34:59 -0400

    fix masonry

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -3,14 +3,20 @@ import type { ImageCardProps } from "../types/ImageCardProps";
 export const images: ImageCardProps[] = [
     {
         src: "/src/assets/gallery/after/IMG_2727.jpg",
-        alt: "Gallery Image",
+        alt: "River bend surrounded by trees",
         aspectRatio: "landscape",
         unedited_src: "/src/assets/gallery/before/IMG_2727.jpg"
     },
     {
         src: "/src/assets/gallery/after/IMG_2732_edited.jpg",
-        alt: "Gallery Image",
+        alt: "White flowers down from a branch",
         aspectRatio: "landscape",
         unedited_src: "/src/assets/gallery/before/IMG_2732_orig.jpg"
+    },
+    {
+        src: "/src/assets/gallery/after/IMG_2712_edited.jpg",
+        alt: "A bench in the woods",
+        aspectRatio: "landscape",
+        unedited_src: "/src/assets/gallery/before/IMG_2712.jpg"
     },
 ];
