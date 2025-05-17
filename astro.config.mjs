0a28c6049c85d7a56e01a792b4cb4cc90a1640dc
rmdluo <rmdluo@gmail.com>
2025-05-16 20:53:43 -0400

    switch to s3 bucket

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  image: {
+    // Example: Allow remote image optimization from a single domain
+    domains: [import.meta.env.VITE_GALLERY_LINK],
+  },
+
+  adapter: vercel()
 });
--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -2,21 +2,21 @@ import type { ImageCardProps } from "../types/ImageCardProps";
 
 export const images: ImageCardProps[] = [
     {
-        src: "/src/assets/gallery/after/IMG_2727.jpg",
+        src: "gallery/after/IMG_2727.jpg",
         alt: "River bend surrounded by trees",
         aspectRatio: "landscape",
-        unedited_src: "/src/assets/gallery/before/IMG_2727.jpg"
+        unedited_src: "gallery/before/IMG_2727.jpg"
     },
     {
-        src: "/src/assets/gallery/after/IMG_2732_edited.jpg",
+        src: "gallery/after/IMG_2732_edited.jpg",
         alt: "White flowers down from a branch",
         aspectRatio: "landscape",
-        unedited_src: "/src/assets/gallery/before/IMG_2732_orig.jpg"
+        unedited_src: "gallery/before/IMG_2732_orig.jpg"
     },
     {
-        src: "/src/assets/gallery/after/IMG_2712_edited.jpg",
+        src: "gallery/after/IMG_2712_edited.jpg",
         alt: "A bench in the woods",
         aspectRatio: "landscape",
-        unedited_src: "/src/assets/gallery/before/IMG_2712.jpg"
+        unedited_src: "gallery/before/IMG_2712.jpg"
     },
 ];
