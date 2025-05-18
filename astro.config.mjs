0e1c7e5a0dbd21c4b9e270f2c45246aefb493fc3
rmdluo <rmdluo@gmail.com>
2025-05-18 04:14:37 -0400

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import vercel from '@astrojs/vercel';
 // https://astro.build/config
-export default defineConfig({
-  integrations: [image({ serviceEntryPoint: "@astrojs/image/sharp" })],
-  
+export default defineConfig({  
   vite: {
     plugins: [tailwindcss()]
   },
