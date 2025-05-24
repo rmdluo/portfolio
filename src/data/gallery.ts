dce4718244e17c36fd54a49d14fa8c505f6e1226
rmdluo <rmdluo@gmail.com>
2025-05-23 21:50:13 -0400

    get rid of some unused stuff

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -4,61 +4,51 @@ export const images: ImageCardProps[] = [
     {
         src: "gallery/after/IMG_2727_edited.jpg",
         alt: "River bend surrounded by trees",
-        aspectRatio: "landscape",
         unedited_src: "gallery/before/IMG_2727_orig.jpg"
     },
     {
         src: "gallery/after/IMG_2732_edited.jpg",
         alt: "White flowers down from a branch",
-        aspectRatio: "portrait",
         unedited_src: "gallery/before/IMG_2732_orig.jpg"
     },
     {
         src: "gallery/after/IMG_2712_edited.jpg",
         alt: "A bench in the woods",
-        aspectRatio: "landscape",
         unedited_src: "gallery/before/IMG_2712_orig.jpg"
     },
     {
         src: "gallery/after/IMG_2780_edited.jpg",
         alt: "A purple flower against the sky",
-        aspectRatio: "portrait",
         unedited_src: "gallery/before/IMG_2780_orig.jpg"
     },
     {
         src: "gallery/after/IMG_2778_edited.jpg",
         alt: "Purple flowers",
-        aspectRatio: "landscape",
         unedited_src: "gallery/before/IMG_2778_orig.jpg"
     },
     {
         src: "gallery/after/IMG_2739_edited.jpg",
         alt: "Light blue flowers",
-        aspectRatio: "landscape",
         unedited_src: "gallery/before/IMG_2739_orig.jpg"
     },
     {
         src: "gallery/after/IMG_2762_edited.jpg",
         alt: "Thing I came across during a walk",
-        aspectRatio: "landscape",
         unedited_src: "gallery/before/IMG_2762_orig.jpg"
     },
     {
         src: "gallery/after/IMG_2698_edited.jpg",
         alt: "Just some daisies",
-        aspectRatio: "portrait",
         unedited_src: "gallery/before/IMG_2698_orig.jpg"
     },
     {
         src: "gallery/after/IMG_2926_edited.jpg",
         alt: "A light purple/blue (I think, I'm colorblind) flower, shaped like a star",
-        aspectRatio: "portrait",
         unedited_src: "gallery/before/IMG_2926_orig.jpg"
     },
     {
         src: "gallery/after/IMG_2927_edited.jpg",
         alt: "A deep purple flower, shaped like a star",
-        aspectRatio: "portrait",
         unedited_src: "gallery/before/IMG_2927_orig.jpg"
     }
 ];
--- src/types/ImageCardProps.ts
+++ src/types/ImageCardProps.ts
@@ -1,11 +1,6 @@
-export type AspectRatio = "square" | "landscape" | "portrait" | "wide" | "ultrawide";
-export type Size = "small" | "medium" | "large" | "full" | "dynamic";
-
 export interface ImageCardProps {
     src: string;
     alt: string;
     caption?: string;
-    aspectRatio?: AspectRatio;
-    size?: Size;
     unedited_src?: string;
 }
