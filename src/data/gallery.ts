fcc3c1f6a06d230e18dcbd12fd8a3168ba68546a
rmdluo <rmdluo@gmail.com>
2025-05-24 22:37:26 -0400

    move files out of specific folders

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -2,53 +2,58 @@ import type { ImageCardProps } from "../types/ImageCardProps";
 
 export const images: ImageCardProps[] = [
     {
-        src: "gallery/after/IMG_2727_edited.jpg",
+        src: "gallery/IMG_2727_edited.jpg",
         alt: "River bend surrounded by trees",
-        unedited_src: "gallery/before/IMG_2727_orig.jpg"
+        unedited_src: "gallery/IMG_2727_orig.jpg"
     },
     {
-        src: "gallery/after/IMG_2732_edited.jpg",
+        src: "gallery/IMG_2732_edited.jpg",
         alt: "White flowers down from a branch",
-        unedited_src: "gallery/before/IMG_2732_orig.jpg"
+        unedited_src: "gallery/IMG_2732_orig.jpg"
     },
     {
-        src: "gallery/after/IMG_2712_edited.jpg",
+        src: "gallery/IMG_2712_edited.jpg",
         alt: "A bench in the woods",
-        unedited_src: "gallery/before/IMG_2712_orig.jpg"
+        unedited_src: "gallery/IMG_2712_orig.jpg"
     },
     {
-        src: "gallery/after/IMG_2780_edited.jpg",
+        src: "gallery/IMG_2780_edited.jpg",
         alt: "A purple flower against the sky",
-        unedited_src: "gallery/before/IMG_2780_orig.jpg"
+        unedited_src: "gallery/IMG_2780_orig.jpg"
     },
     {
-        src: "gallery/after/IMG_2778_edited.jpg",
+        src: "gallery/IMG_2778_edited.jpg",
         alt: "Purple flowers",
-        unedited_src: "gallery/before/IMG_2778_orig.jpg"
+        unedited_src: "gallery/IMG_2778_orig.jpg"
     },
     {
-        src: "gallery/after/IMG_2739_edited.jpg",
+        src: "gallery/IMG_2739_edited.jpg",
         alt: "Light blue flowers",
-        unedited_src: "gallery/before/IMG_2739_orig.jpg"
+        unedited_src: "gallery/IMG_2739_orig.jpg"
     },
     {
-        src: "gallery/after/IMG_2762_edited.jpg",
+        src: "gallery/IMG_2762_edited.jpg",
         alt: "Thing I came across during a walk",
-        unedited_src: "gallery/before/IMG_2762_orig.jpg"
+        unedited_src: "gallery/IMG_2762_orig.jpg"
     },
     {
-        src: "gallery/after/IMG_2698_edited.jpg",
+        src: "gallery/IMG_2698_edited.jpg",
         alt: "Just some daisies",
-        unedited_src: "gallery/before/IMG_2698_orig.jpg"
+        unedited_src: "gallery/IMG_2698_orig.jpg"
     },
     {
-        src: "gallery/after/IMG_2926_edited.jpg",
+        src: "gallery/IMG_2926_edited.jpg",
         alt: "A light purple/blue (I think, I'm colorblind) flower, shaped like a star",
-        unedited_src: "gallery/before/IMG_2926_orig.jpg"
+        unedited_src: "gallery/IMG_2926_orig.jpg"
     },
     {
-        src: "gallery/after/IMG_2927_edited.jpg",
+        src: "gallery/IMG_2927_edited.jpg",
         alt: "A deep purple flower, shaped like a star",
-        unedited_src: "gallery/before/IMG_2927_orig.jpg"
+        unedited_src: "gallery/IMG_2927_orig.jpg"
+    },
+    {
+        src: "gallery/IMG_2947_edited.jpg",
+        alt: "Cold imposing power lines against a cloudy sky",
+        unedited_src: "gallery/IMG_2947_orig.jpg"
     }
 ];
